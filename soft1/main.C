68273683c8a5fbb2c9693431f5f14a1f40cc914d
nekelanchlan <nekelanchlan@gmail.com>
2024-09-05 12:29:05 +0800

    三角形作业

--- soft1/main.C
+++ soft1/main.C
@@ -1,6 +1,24 @@
-#include <stdio.h>
-int main()
-{
-    printf("hello");
-    return 0;
+#include<stdio.h>
+int main(){
+float a,b,c;
+printf("请输入三角形的三条边：\n");
+scanf("%f,%f,%f",&a,&b,&c);
+if(a+b>c&&b+c>a&&c+a>b) {
+	if(a==b&&b==c){
+		printf("该三角形是等边三角形\n");
+	}
+	else if(a==b||b==c||c==a){
+			printf("该三角形是等腰三角形\n");
+	}
+	else if(a*a+b*b==c*c||b*b+c*c==a*a||a*a+c*c==b*b){
+		printf("该三角形是直角三角形\n"); 
+	}
+		else{
+			printf("该三角形是普通三角形\n");
+		} 
+    }
+    else{
+        printf("非三角形\n");
+	}
+	return 0; 
 }
